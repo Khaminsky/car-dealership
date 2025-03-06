e0fa34f59480e8221c7e22ea1d6109434a0daaa3
Khaminsky <kkapepeta@gmail.com>
2025-03-06 21:05:30 +0200

    Fixes font optimization

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  optimizeFonts: false, // Disable Google Fonts optimization
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
-    ], // Add Unsplash domain
+    ],
   },
 };
 
